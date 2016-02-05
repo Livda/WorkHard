e005645b91e14845caa7e339d254220e091ff960
Aurélien Fontaine <aurelien.fontaine@insa-rennes.fr>
2016-02-05 17:21:35 +0000

    work on the add window

--- cit/OOP/workspace/AnimalFundation/src/gui/MainWindow.java
+++ cit/OOP/workspace/AnimalFundation/src/gui/MainWindow.java
@@ -1,16 +1,16 @@
 package gui;
 
+import controlers.MainControler;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
@@ -21,7 +21,7 @@ public class MainWindow extends Application {
 			primaryStage.setTitle("Animal Shelter");
 			
 			BorderPane mainLayout = new BorderPane();
-			Scene scene = new Scene(mainLayout,400,400);
+			Scene scene = new Scene(mainLayout,800,600);
 			
 			Menu fileMenu = new Menu("File");
 			MenuItem newItem = new MenuItem("New ...");
@@ -69,7 +69,96 @@ public class MainWindow extends Application {
 	        mainButtons.setAlignment(Pos.TOP_CENTER);
 	        mainButtons.getChildren().addAll(adoption, lost, found);
 	        
+	        GridPane personGrid = new GridPane();
+	        personGrid.setVgap(10);
+	        personGrid.setHgap(10);
+	        Text personName = new Text("Name :");
+	        TextField setPersonName = new TextField();
+	        Text personAdress = new Text("Adress :");
+	        TextArea setPersonAdress = new TextArea();
+	        setPersonAdress.setMaxSize(300, 25);
+	        Text personPhone = new Text("Phone :");
+	        TextField setPersonPhone = new TextField();
+	        Text personEmail = new Text("Email :");
+	        TextField setPersonEmail = new TextField();
+	        
+	        personGrid.add(personName, 0, 0);
+	        personGrid.add(setPersonName, 1, 0);
+	        personGrid.add(personAdress, 0, 1);
+	        personGrid.add(setPersonAdress, 1, 1);
+	        personGrid.add(personPhone, 0, 2);
+	        personGrid.add(setPersonPhone, 1, 2);
+	        personGrid.add(personEmail, 0, 3);
+	        personGrid.add(setPersonEmail, 1, 3);
+	        
+	        VBox personBox = new VBox(20);
+	        Text personTitle = new Text("Person");
+	        personTitle.setFont(Font.font(null, FontWeight.BOLD, 25));
+	        personBox.getChildren().addAll(personTitle, personGrid);
+	        personBox.setAlignment(Pos.TOP_CENTER);
+	        
+	        GridPane animalGrid = new GridPane();
+	        animalGrid.setVgap(10);
+	        animalGrid.setHgap(10);
+	        Text animalName = new Text("Name :");
+	        TextField setAnimalName = new TextField();
+	        Text animalAge = new Text("Age :");
+	        TextField setAnimalAge = new TextField();
+	        Text animalColor = new Text("Color :");
+	        TextField setAnimalColor = new TextField();
+	        Text animalGender = new Text("Gender :");
+	        ToggleGroup group = new ToggleGroup();
+	        RadioButton maleBox = new RadioButton("Male");
+	        maleBox.setToggleGroup(group);
+	        maleBox.setSelected(true);
+	        RadioButton femaleBox = new RadioButton("Female");
+	        femaleBox.setToggleGroup(group);
+	        HBox gender = new HBox(15);
+	        gender.getChildren().addAll(maleBox, femaleBox);
+	        Text animalDescription = new Text("Description :");
+	        TextArea description = new TextArea();
+	        description.setMaxSize(300, 50);
+	        Text animalBreed = new Text("Breed :");
+	        TextField setAnimalBreed = new TextField();
+	        Text animalType = new Text("Type :");
+	        TextField setAnimalType = new TextField();
+	        
+	        animalGrid.add(animalName, 0, 0);
+	        animalGrid.add(setAnimalName, 1, 0);
+	        animalGrid.add(animalAge, 0, 1);
+	        animalGrid.add(setAnimalAge, 1, 1);
+	        animalGrid.add(animalColor, 0, 2);
+	        animalGrid.add(setAnimalColor, 1, 2);
+	        animalGrid.add(animalDescription, 0, 3);
+	        animalGrid.add(description, 1, 3);
+	        animalGrid.add(animalBreed, 0, 4);
+	        animalGrid.add(setAnimalBreed, 1, 4);
+	        animalGrid.add(animalType, 0, 5);
+	        animalGrid.add(setAnimalType, 1, 5);
+	        animalGrid.add(animalGender, 0, 6);
+	        animalGrid.add(gender, 1, 6);
+	        gender.setAlignment(Pos.CENTER);
+	        
+	        VBox animalBox = new VBox(20);
+	        Text animalTitle = new Text("Animal");
+	        animalTitle.setFont(Font.font(null, FontWeight.BOLD, 25));
+	        animalBox.getChildren().addAll(animalTitle, animalGrid);
+	        animalBox.setAlignment(Pos.TOP_CENTER);
+	        
+	        HBox bottomButtons = new HBox(10);
+	        Button ok = new Button("OK");
+	        Button cancel = new Button("Cancel");
+	        bottomButtons.getChildren().addAll(ok, cancel);
+	        bottomButtons.setAlignment(Pos.BOTTOM_CENTER);
+	        
 	        secondLayout.setTop(mainButtons);
+	        BorderPane.setMargin(mainButtons, new Insets(10));
+	        secondLayout.setLeft(personBox);
+	        BorderPane.setMargin(personBox, new Insets(10));
+	        secondLayout.setRight(animalBox);
+	        BorderPane.setMargin(animalBox, new Insets(10));
+	        secondLayout.setBottom(bottomButtons);
+	        BorderPane.setMargin(bottomButtons, new Insets(10));
 	        mainLayout.setCenter(secondLayout);
 	        
 			primaryStage.setScene(scene);
@@ -81,6 +170,8 @@ public class MainWindow extends Application {
 	}
 
 	public static void main(String[] args) {
+		MainControler main = new MainControler();
+		main.start();
 		launch(args);
 	}
 }
