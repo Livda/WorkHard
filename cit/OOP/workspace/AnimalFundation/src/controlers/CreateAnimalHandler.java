0dfc45ad4e1a04a43bd0a15a7026e0adc32d37f7
Livda <aurelien.fontaine@insa-rennes.fr>
2016-03-01 11:06:01 +0000

    trouble to access to the TableView

--- cit/OOP/workspace/AnimalFundation/src/controlers/CreateAnimalHandler.java
+++ cit/OOP/workspace/AnimalFundation/src/controlers/CreateAnimalHandler.java
@@ -5,6 +5,7 @@ package controlers;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import api.Adoption;
 import api.Animal;
@@ -22,11 +23,14 @@ import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * @author Aurelien Fontaine
@@ -115,9 +119,22 @@ public class CreateAnimalHandler implements EventHandler<ActionEvent>{
 		node = getNodeByRowColumnIndex(6, 1, animal);
 		HBox buttonBox = ((HBox)node);
 		boolean gender  = ((RadioButton)buttonBox.getChildren().get(0)).isSelected();
-		Animal a = new Animal(age, colour, gender, description, aName, null, breed, category, type);
-		
+		Animal a = new Animal(age, colour, gender, description, aName, null, breed, 
+				category, type);
+		//add the animal to the right list
 		aList.add(a);
+		
+		//we add the new animal to the TableView
+		node = animal.getParent().getParent().getParent().getParent().getParent();
+		BorderPane pane = (BorderPane) node;
+		VBox vbox = (VBox) pane.getLeft();
+		@SuppressWarnings("unchecked")
+		TableView<Animal> table = (TableView<Animal>) vbox.getChildren().get(1);
+		ArrayList<Animal> list = shelter.getAllAnimals();
+		ObservableList<Animal> allData = table.getItems();
+		allData.clear();
+		allData.addAll(list);
+		table.setItems(allData);
 	}
 	
 	@SuppressWarnings("static-access")
