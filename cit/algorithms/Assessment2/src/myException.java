e6b35e52f2441207be905a19e0c0324329a6b7df
Livda <aurelien.fontaine@insa-rennes.fr>
2016-04-13 11:55:58 +0100

    add all the javadoc

--- cit/algorithms/Assessment2/src/myException.java
+++ cit/algorithms/Assessment2/src/myException.java
@@ -1,15 +1,25 @@
-
+/**
+ * {@code MyException} is an exception create for the needs of the labs
+ * @author Ignacio Castiñeiras
+ */
 public class myException extends java.lang.RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	//--------------------------------------------------
-	// Constructor
-	//--------------------------------------------------
+	/**
+	 * Constructs an <code>myException</code> with no 
+	 * detail message.
+	 */
 	public myException(){
 		super("ADT Operation Error");
 	}
 	
+	/**
+     * Constructs an <code>myException</code> with the
+     * specified detail message.
+     *
+     * @param   s   the detail message.
+     */
 	public myException(String s){
 		super(s);
 	}
