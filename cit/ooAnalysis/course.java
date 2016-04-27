8823b279ff6f74fe6e4d5e053dc52e8d4fff565c
Livda <aurelien.fontaine@insa-rennes.fr>
2016-04-27 12:44:18 +0100

    add few things to learn

--- cit/ooAnalysis/course.java
+++ cit/ooAnalysis/course.java
@@ -27,3 +27,13 @@ public class RoarSound implements ISoundBehaviour {
 }
 
 //Delegation is used when we want to use SOME of the methods of a class.
+
+//See law of Demeter -> Only talk to your immediate friends
+/*
+1) can call his own methods
+2) can call methods of all object we instantiate
+3) can call methods of attributes
+4) can call methods of parameters
+*/
+
+//A class should know few classes
